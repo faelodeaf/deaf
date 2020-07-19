464b4d98dc52987fc51a747fb45dc962728c0989
Woolf <29702137+S0Ulle33@users.noreply.github.com>
2020-07-19 19:34:10 +0600

    Update utils.py
    Currently there's no evidence that we should strip "Login to " from
    realm to succesfully login, more testing needed.

--- modules/utils.py
+++ modules/utils.py
@@ -82,7 +82,7 @@ def detect_auth_method(target):
         end = data.find('"', begin)
         if "Login to " in data[begin:end]:
             logger.debug(f"{get_camera_rtsp_url(target)} has LOGIN TO")
-        return data[begin:end].lstrip("Login to ")
+        return data[begin:end]
 
     data = str(target._data)
 

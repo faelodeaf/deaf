c39a25309466c3e95ea1d8f9804d8bf2769204f1
Woolf <29702137+S0Ulle33@users.noreply.github.com>
2020-07-19 19:25:30 +0600

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ from typing import List
 
 CHECK_THREADS: int = 500
 BRUTE_THREADS: int = 200
-SCREENSHOT_THREADS: int = 50
+SCREENSHOT_THREADS: int = 10 # Less threads leads to more successful screenshots
 
 PORT: int = 554
 SOCKET_TIMEOUT: int = 2
@@ -16,8 +16,8 @@ TARGETS: List[str] = []
 
 
 start_datetime = time.strftime("%Y.%m.%d-%H.%M.%S")
-DEBUG_LOG_FILE = Path.cwd() / 'debug.log'
-REPORT_FOLDER = Path.cwd() / 'reports' / start_datetime
-PICS_FOLDER = REPORT_FOLDER / 'pics'
-RESULT_FILE = REPORT_FOLDER / 'result.txt'
-HTML_FILE = REPORT_FOLDER / 'index.html'
+DEBUG_LOG_FILE = Path.cwd() / "debug.log"
+REPORT_FOLDER = Path.cwd() / "reports" / start_datetime
+PICS_FOLDER = REPORT_FOLDER / "pics"
+RESULT_FILE = REPORT_FOLDER / "result.txt"
+HTML_FILE = REPORT_FOLDER / "index.html"
